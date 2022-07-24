33c213d938c93d45d775d62107939361a128fa87
Jake Readman <jakereadman@gmail.com>
2022-07-24 20:10:12 +0100

    add tests

--- src/index.js
+++ src/index.js
@@ -1,21 +1,6 @@
-const express = require("express");
-require("./db/mongoose");
-const userRouter = require("./routers/user");
-const taskRouter = require("./routers/task");
-const bcrypt = require("bcryptjs");
+const app = require("./app");
 
-const app = express();
 const port = process.env.PORT;
-
-// app.use((req, res, next) => {
-//   res
-//     .status(503)
-//     .send({ message: "Sorry, the site is currently down for maintencance" });
-// });
-
-app.use(express.json());
-app.use(userRouter);
-app.use(taskRouter);
 
 app.listen(port, () => {
     console.log(`Server is listening on http://localhost:${port}`);
